325c23db008f81a59737e963a702b0fa5a2df04a
shunpcho <kshuchi0203@gmail.com>
2026-08-10 19:01:46 +0900

    fix: model init

--- src/image_restoration_allinone/models/__init__.py
+++ src/image_restoration_allinone/models/__init__.py
@@ -1,5 +1,3 @@
-"""Models module."""
-
-from image_restoration_allinone.models.nafnet.network import build_model, NAFNet
-
-__all__ = ["NAFNet", "build_model"]
+from image_restoration_allinone.models.build import build_model as build_model
+from image_restoration_allinone.models.build import MODEL_REGISTRY as MODEL_REGISTRY
+from image_restoration_allinone.models.nafnet.network import NAFNet as NAFNet
--- src/image_restoration_allinone/models/nafnet/__init__.py
+++
@@ -1,3 +0,0 @@
-from image_restoration_allinone.models.build import build_model as build_model
-from image_restoration_allinone.models.build import MODEL_REGISTRY as MODEL_REGISTRY
-from image_restoration_allinone.models.nafnet.network import NAFNet as NAFNet
